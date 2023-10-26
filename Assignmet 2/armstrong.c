dd3ea854b4385d2730a1826b538947cd2e0ad54a
MohammedFSubhi <147100391+MohammedFSubhi@users.noreply.github.com>
2023-10-26 23:02:37 +0300

    Add files via upload

---
+++ Assignmet 2/armstrong.c
@@ -0,0 +1,43 @@
+// Assignment 2 
+// Mohammed Faisal Subhi
+// 2035752
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+int ArmstrongC = 0;
+int NumbersC = 0;
+int multiplay;
+int result;
+int quotient; 
+int count =0;
+while (ArmstrongC < 20){
+     count = 0;
+     quotient = NumbersC;
+    while (quotient != 0){ 
+        quotient = quotient/10;
+        count++;
+    }
+    int cnt = count;
+    result = 0;
+    multiplay =1;
+    quotient = NumbersC;
+    while (quotient !=0 ){
+        int rem =  quotient%10;
+        while (cnt != 0){
+            multiplay = multiplay*rem;
+            cnt--;
+        }
+        result = result + multiplay;
+        cnt = count;
+        quotient = quotient/10;
+        multiplay =1;
+    }
+    if (result == NumbersC){
+        printf("Armstrong [ %d %s %d \n", ++ArmstrongC , "]: ",   NumbersC);
+    } 
+    NumbersC++;
+} 
+return 0;
+}
+
